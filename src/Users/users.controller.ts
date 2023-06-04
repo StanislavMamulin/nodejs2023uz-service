9a5de600fc7c2467bfad4cced0f0b9107ff3774a
Stanislav Mamulin <StanislavPM@ya.ru>
2023-06-04 19:43:22 +0500

    feat: implement actions in user controller

--- src/Users/users.controller.ts
+++ src/Users/users.controller.ts
@@ -6,28 +6,64 @@ import {
   Post,
   Put,
   Body,
+  HttpCode,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 import { UserResponse } from './interfaces/user.interface';
+import { UserService } from './users.service';
+import { ErrorHandler } from 'src/Errors/ErrorHandler';
 
 @Controller('user')
 export class UsersController {
+  constructor(private userService: UserService) {}
+
   @Get()
-  getAllUsers(): UserResponse[] {}
+  getAllUsers(): UserResponse[] {
+    try {
+      return this.userService.getAllUsers();
+    } catch (error: unknown) {
+      ErrorHandler(error);
+    }
+  }
 
   @Get(':id')
-  getOneUser(@Param('id') id: string): UserResponse {}
+  getOneUser(@Param('id') id: string): UserResponse {
+    try {
+      return this.userService.getUserById(id);
+    } catch (error) {
+      ErrorHandler(error);
+    }
+  }
 
   @Post()
-  createUser(@Body() createUserDto: CreateUserDto): UserResponse {}
+  createUser(@Body() createUserDto: CreateUserDto): UserResponse {
+    try {
+      return this.userService.create(createUserDto);
+    } catch (error) {
+      ErrorHandler(error);
+    }
+  }
 
   @Put(':id')
   updatePassword(
     @Param('id') id: string,
     @Body() updatePasswordDto: UpdatePasswordDto,
-  ): UserResponse {}
+  ): UserResponse {
+    try {
+      return this.userService.updatePassword(id, updatePasswordDto);
+    } catch (error) {
+      ErrorHandler(error);
+    }
+  }
 
   @Delete(':id')
-  deleteUser(@Param('id') id: string) {}
+  @HttpCode(204)
+  deleteUser(@Param('id') id: string) {
+    try {
+      this.userService.deleteUser(id);
+    } catch (error) {
+      ErrorHandler(error);
+    }
+  }
 }
