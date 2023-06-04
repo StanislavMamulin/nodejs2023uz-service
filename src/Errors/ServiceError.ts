3710fefb8951cb6c3b6bdcbb4ff19066a6868e50
Stanislav Mamulin <StanislavPM@ya.ru>
2023-06-04 18:54:52 +0500

    feat: add custom service methods errors

---
+++ src/Errors/ServiceError.ts
@@ -0,0 +1,43 @@
+import { ErrorMessages } from './errorMessages';
+
+enum ErrorCodes {
+  INVALID_ID,
+  USER_NOT_FOUND,
+  REQUIRED_FIELDS_IS_MISSED,
+  WRONG_PASSWORD,
+}
+
+export class ServiceError extends Error {
+  message: string;
+  code: ErrorCodes;
+
+  constructor(message: string, code: number) {
+    super(message);
+    this.message = message;
+    this.code = code;
+  }
+}
+
+export class InvalidIdError extends ServiceError {
+  constructor(message: string = ErrorMessages.INVALID_ID) {
+    super(message, ErrorCodes.INVALID_ID);
+  }
+}
+
+export class UserNotFoundError extends ServiceError {
+  constructor(message: string = ErrorMessages.USER_NOT_FOUND) {
+    super(message, ErrorCodes.USER_NOT_FOUND);
+  }
+}
+
+export class RequiredFieldsIsMissedError extends ServiceError {
+  constructor(message: string = ErrorMessages.REQUIRED_FIELDS_IS_MISSED) {
+    super(message, ErrorCodes.REQUIRED_FIELDS_IS_MISSED);
+  }
+}
+
+export class WrongPasswordError extends ServiceError {
+  constructor(message: string = ErrorMessages.WRONG_PASSWORD) {
+    super(message, ErrorCodes.WRONG_PASSWORD);
+  }
+}
---
+++ src/Errors/errorMessages.ts
@@ -0,0 +1,6 @@
+export enum ErrorMessages {
+  USER_NOT_FOUND = 'User not found',
+  INVALID_ID = 'Incorrect user id format',
+  REQUIRED_FIELDS_IS_MISSED = 'Required body fields is missed',
+  WRONG_PASSWORD = 'Wrong old password',
+}
