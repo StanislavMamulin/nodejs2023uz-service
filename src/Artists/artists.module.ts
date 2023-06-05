8dcd1dc2e0758d025f3ccf0de39c5135b277003e
Stanislav Mamulin <StanislavPM@ya.ru>
2023-06-05 23:47:38 +0500

    feat: add artist resource

---
+++ src/Artists/artists.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ArtistsService } from './artists.service';
+import { ArtistsController } from './artists.controller';
+import { DBService } from 'src/DB/DB.service';
+
+@Module({
+  controllers: [ArtistsController],
+  providers: [ArtistsService, DBService],
+})
+export class ArtistsModule {}
