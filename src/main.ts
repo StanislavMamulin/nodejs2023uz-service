42c97252a9f21dc403127ec504d6012c4489f9a6
Stanislav Mamulin <StanislavPM@ya.ru>
2023-06-12 12:44:57 +0500

    build: add and init prisma

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +11,10 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  dotenv.config();
+
+  const env = dotenv.config();
+  dotenvExpand.expand(env);
+
   await app.listen(process.env.PORT);
   console.log('Service started on port', process.env.PORT);
 }
